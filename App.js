12cf9caba28d6916ca041916519b3ca42b1f178a
ruiiss <ariqrifqy15z@gmail.com>
2026-07-27 14:10:02 +0700

    fix: everything

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
-import 'react-native-gesture-handler';
 
 export default function App() {
   return (
